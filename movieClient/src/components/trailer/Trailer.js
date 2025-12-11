6237f6187ab0d3d365438e0ce66168bb06f2973a
Sacihn00028 <sachinpant0028@gmail.com>
2025-12-11 13:44:47 +0530

    Add backend and frontend source code
    - Add Spring Boot movies backend with controllers, services, and repositories
    - Add React movieClient frontend with components
    - Include Maven and npm configuration files
    - Respect .gitignore rules for generated and build artifacts

---
+++ movieClient/src/components/trailer/Trailer.js
@@ -0,0 +1,19 @@
+import {useParams} from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import './Trailer.css';
+
+import React from 'react';
+
+const Trailer = () => {
+    const params = useParams();
+    const key = params.ytTrailerId;
+
+  return (
+    <div className="react-player-container">
+      {(key!=null)?<ReactPlayer controls={true} playing={true} url ={`https://youtu.be/${key}`} 
+      width = '100%' height='100%' />:null}
+    </div>
+  )
+}
+
+export default Trailer
